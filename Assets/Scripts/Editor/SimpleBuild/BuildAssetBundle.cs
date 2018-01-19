67abc461d5be6fbf0121cfd7753ebfdd9bfe2b93
Tetsuya Mori <monry84@gmail.com>
2018-01-19 16:07:26 +0900

    :shirt: Refactor BuildTarget enums

--- Assets/Scripts/Editor/SimpleBuild/BuildAssetBundle.cs
+++ Assets/Scripts/Editor/SimpleBuild/BuildAssetBundle.cs
@@ -19,16 +19,12 @@ namespace SimpleBuild {
         /// ビルドターゲットと出力先ディレクトリの辞書
         /// </summary>
         private static readonly Dictionary<BuildTarget, string> OUTPUT_DIRECTORY_MAP = new Dictionary<BuildTarget, string>() {
-            { BuildTarget.iOS                     , "iOS" },
-            { BuildTarget.Android                 , "Android" },
-            { BuildTarget.StandaloneWindows       , "Standalone" },
-            { BuildTarget.StandaloneWindows64     , "Standalone" },
-            { BuildTarget.StandaloneOSXIntel      , "Standalone" },
-            { BuildTarget.StandaloneOSXIntel64    , "Standalone" },
-            { BuildTarget.StandaloneOSXUniversal  , "Standalone" },
-            { BuildTarget.StandaloneLinux         , "Standalone" },
-            { BuildTarget.StandaloneLinux64       , "Standalone" },
-            { BuildTarget.StandaloneLinuxUniversal, "Standalone" },
+            { BuildTarget.iOS                , "iOS" },
+            { BuildTarget.Android            , "Android" },
+            { BuildTarget.StandaloneWindows  , "Standalone" },
+            { BuildTarget.StandaloneWindows64, "Standalone" },
+            { BuildTarget.StandaloneOSX      , "Standalone" },
+            { BuildTarget.StandaloneLinux    , "Standalone" },
         };
 
         /// <summary>
@@ -66,10 +62,8 @@ namespace SimpleBuild {
         /// <remarks>Jenkins などの CI ツールからのキックを想定してサフィックスをつけています。</remarks>
         public static void Run_Standalone() {
             new BuildAssetBundle() {
-#if UNITY_EDITOR_OSX && UNITY_EDITOR_64
-                BuildTarget = BuildTarget.StandaloneOSXIntel64,
-#elif UNITY_EDITOR_OSX && !UNITY_EDITOR_64
-                BuildTarget = BuildTarget.StandaloneOSXIntel,
+#if UNITY_EDITOR_OSX
+                BuildTarget = BuildTarget.StandaloneOSX,
 #elif UNITY_EDITOR_WIN && UNITY_EDITOR_64
                 BuildTarget = BuildTarget.StandaloneWindows64,
 #elif UNITY_EDITOR_WIN && !UNITY_EDITOR_64
